e0ac5fa462b17efb3ffaa73b6758cfd01dd8e955
Nguyen Tien Thanh - ThanhNT90 (FGA.DAP) <ThanhNT90@fsoft.com.vn>
2021-04-11 23:01:38 +0700

    test: call to sound device
    - Add conference to create audio flow from call to sound device
    
    Signed-off-by: Nguyen Tien Thanh - ThanhNT90 (FGA.DAP) <ThanhNT90@fsoft.com.vn>

--- main.c
+++ main.c
@@ -28,8 +28,39 @@
 
  #define SIP_USER_2 "996892"
  #define SIP_PASSWD_2 "Pin996892"
+
+ #define SND_DEV_IDX_1 14
  
- 
+// cut chuoi string
+
+int find(char *src, char *key){
+  int c;
+  int b;
+  char *str;
+  str = strstr(src,key);
+  if (str != NULL){
+    c = (int) src;
+    b = (int) str;
+  return b-c;
+  }
+  else return -1;
+}
+char *trim(char *src, int start, int end){
+  char *val;
+  val = malloc(end-start+1);
+  int i;
+  for (i = start; i < end; i++){
+    val[i-start] = (char)src[i]; 
+  }
+  return val;
+}
+pj_str_t cat_chuoi(char *src){
+  pj_str_t val;
+  char *temp;
+  temp = trim(src, find(src, ":") + 1, find(src, "@"));
+  val = pj_str(temp);
+}
+
  /* Callback called by the library upon receiving incoming call */
  static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id,
   pjsip_rx_data *rdata)
@@ -66,14 +97,69 @@
  static void on_call_media_state(pjsua_call_id call_id)
  {
   pjsua_call_info ci;
+  pj_str_t a;
  
   pjsua_call_get_info(call_id, &ci);
- 
-  if (ci.media_status == PJSUA_CALL_MEDIA_ACTIVE) {
-  // When media is active, connect call to sound device.
-  pjsua_conf_connect(ci.conf_slot, 0);
-  pjsua_conf_connect(0, ci.conf_slot);
-  }
+  printf("=================pjsua_acc_id = %d \n", ci.acc_id);
+  printf("=================local_contact = %s \n",ci.local_contact);
+  printf("=================remote_contact = %s \n", ci.remote_contact);
+  a = cat_chuoi(ci.local_contact.ptr);
+  printf("=========================a = %s\n", a);
+
+  //When media is active, connect call to sound device.
+  //pjsua_get_conference() // get_master_port
+  if (ci.media_status == PJSUA_CALL_MEDIA_ACTIVE && (strcmp(a.ptr, SIP_USER_1) == 0)) {
+	  if (strcmp(a.ptr, SIP_USER_1) == 0) {
+  	 	printf("===========================match phone 1");
+
+                pjmedia_conf *conf_1 = NULL; // tao 1 pj media NULL
+		pj_pool_t *pjsua_pool_1 = NULL;
+  		pjsua_pool_1 = pjsua_pool_create("phone_pool_1", 512, 512); // tao pool de su dung
+		pj_status_t status;
+
+		//pjmedia_pool_1 =
+		pjmedia_port *mp_1 = NULL; 
+	        status = pjmedia_conf_create(pjsua_pool_1, 3, 16000, 1, 320, 16, PJMEDIA_CONF_NO_DEVICE, &conf_1); // tao ra 1 pj media conf
+		if (status != PJ_SUCCESS) printf("Error in pjmedia_conf_create\n");
+		mp_1 = pjmedia_conf_get_master_port(conf_1); // lay master port tu conf vua tao
+
+		// tap ra 1 port media NULL va gan port do vao conf o tren
+		pjmedia_port *sc_1 = NULL;
+		unsigned int sc_1_slot;
+	        status = pjmedia_conf_add_port(conf_1, pjsua_pool_1, sc_1, NULL, &sc_1_slot);
+		if (status != PJ_SUCCESS) printf("Error in pjmedia_conf_add_port\n");
+
+		// tao ra sound device port tu device index
+		pjmedia_snd_port *snd_port_1 = NULL;
+		status = pjmedia_snd_port_create(pjsua_pool_1, SND_DEV_IDX_1, SND_DEV_IDX_1, 8000, 2, 320, 16, 0, &snd_port_1);
+		if (status != PJ_SUCCESS) printf("Error in pjmedia_snd_port_create");
+
+                // connect port master cua conference voi port cua sound device 
+		status = pjmedia_snd_port_connect(snd_port_1, mp_1);
+		if (status != PJ_SUCCESS) printf("Error in pjmedia_snd_port_connect");
+		
+		// connect port cho sound device va port sc_1
+		pjmedia_conf_connect_port(conf_1, sc_1, 0, 128);
+		pjmedia_conf_connect_port(conf_1, 0, sc_1, 128);
+
+		// Phan conf cho pjsua
+		// lay ra source slot tu call id
+		pjsua_conf_port_id source_slot = pjsua_call_get_conf_port(ci.id);
+		// gan source slot day vao conf cua pjsua
+		pjsua_conf_port_id p_id;
+		pjsua_conf_connect(p_id, source_slot);
+
+		// Phan gan p_id va sc_1
+		//pjsua_conf_add_port(pjsua_pool_1, sc_1, &p_id);
+	//	pjsua_conf_connect(p_id, source_slot);
+	//        pjsua_conf_connect(source_slot, p_id);
+
+	  } else {
+	        printf("===========================match phone 2");
+               // pjsua_conf_connect(ci.conf_slot, 0);
+               // pjsua_conf_connect(0, ci.conf_slot);
+	  }
+  	}
  }
  
  /* Display error and exit application */
@@ -91,19 +177,18 @@
   */
  int main(int argc, char *argv[])
  {
-  pjsua_acc_id acc_id[2];
+  pjsua_acc_id acc_id;
   pj_status_t status;
+
+  // init PJLIB
+  status = pj_init();
+  PJ_ASSERT_RETURN(status == PJ_SUCCESS, 1);
  
   /* Create pjsua first! */
   status = pjsua_create();
   if (status != PJ_SUCCESS) error_exit("Error in pjsua_create()", status);
- 
-  /* If argument is specified, it's got to be a valid SIP URL */
-  if (argc > 1) {
-  status = pjsua_verify_url(argv[1]);
-  if (status != PJ_SUCCESS) error_exit("Invalid URL in argv", status);
-  }
- 
+
+
   /* Init pjsua */
   {
   pjsua_config cfg;
@@ -151,7 +236,6 @@
   cfg[0].cred_info[0].data_type = PJSIP_CRED_DATA_PLAIN_PASSWD;
   cfg[0].cred_info[0].data = pj_str(SIP_PASSWD_1);
  
-
   cfg[1].id = pj_str("sip:" SIP_USER_2 "@" SIP_DOMAIN);
   cfg[1].reg_uri = pj_str("sip:" SIP_DOMAIN);
   cfg[1].cred_count = 1;
@@ -162,19 +246,33 @@
   cfg[1].cred_info[0].data = pj_str(SIP_PASSWD_2);
 
 
-  status = pjsua_acc_add(&cfg[0], PJ_TRUE, &acc_id[0]);
+  status = pjsua_acc_add(&cfg[0], PJ_TRUE, &acc_id);
   if (status != PJ_SUCCESS) error_exit("Error adding account 0", status);
-  status = pjsua_acc_add(&cfg[1], PJ_TRUE, &acc_id[1]);
+  status = pjsua_acc_add(&cfg[1], PJ_TRUE, &acc_id);
   if (status != PJ_SUCCESS) error_exit("Error adding account 1", status);
   }
  
   /* If URL is specified, make call to the URL. */
-  if (argc > 1) {
-  pj_str_t uri = pj_str(argv[1]);
-  status = pjsua_call_make_call(acc_id[0], &uri, 0, NULL, NULL, NULL);
-  status = pjsua_call_make_call(acc_id[1], &uri, 0, NULL, NULL, NULL);
+  // thanhnt: make 1 call succees
+  /*
+  PJ_LOG(1, (__FILE__, "Make call now"));
+  pjsua_call_id call_id = PJSUA_INVALID_ID;
+  char phone_c[100];
+  pjsua_msg_data msg_data;
+  pj_ansi_sprintf(phone_c, "<sip:%s@%s:%d>", "522601", "192.168.1.198", 5060);
+  pj_str_t dst_uri = pj_str(phone_c);
+
+  pjsua_msg_data_init(&msg_data);
+  status = pjsua_call_make_call(acc_id, &dst_uri, 0, NULL, &msg_data, &call_id);
   if (status != PJ_SUCCESS) error_exit("Error making call", status);
-  }
+  */
+  // thanhnt
+
+  //if (argc > 1) {
+  //pj_str_t uri = pj_str(argv[1]);
+  //status = pjsua_call_make_call(acc_id, &uri, 0, NULL, NULL, NULL);
+  //if (status != PJ_SUCCESS) error_exit("Error making call", status);
+  //}
  
   /* Wait until user press "q" to quit. */
   for (;;) {
