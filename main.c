d7af2ca0e1068c74705498729153bb34efc782df
Nguyen Tien Thanh - ThanhNT90 (FGA.DAP) <ThanhNT90@fsoft.com.vn>
2021-04-17 22:07:10 +0700

    main: add config for 4 calls
    - add config to register 4 calls
    - add macro for soundevice index, which can be diferent
    on each board and sound device
    
    Signed-off-by: Nguyen Tien Thanh - ThanhNT90 (FGA.DAP) <ThanhNT90@fsoft.com.vn>

--- main.c
+++ main.c
@@ -3,8 +3,19 @@
 #define THIS_FILE "APP" 
 
 #define SIP_DOMAIN "192.168.1.198"
-#define SIP_USER "996891"
-#define SIP_PASSWD "Pin996891"  
+#define SIP_USER_0 "996891"
+#define SIP_PASSWD_0 "Pin996891"
+#define SIP_USER_1 "996892"
+#define SIP_PASSWD_1 "Pin996892"
+#define SIP_USER_2 "996893"
+#define SIP_PASSWD_2 "Pin996893"
+#define SIP_USER_3 "996894"
+#define SIP_PASSWD_3 "Pin996894"
+
+#define SND_DEV_IDX_1 8
+#define SND_DEV_IDX_2 12
+#define SND_DEV_IDX_3 16
+#define SND_DEV_IDX_4 20
 
 /* Display error and exit application */
 static void error_exit(const char *title, pj_status_t status)
@@ -12,7 +23,7 @@ static void error_exit(const char *title, pj_status_t status)
     pjsua_perror(THIS_FILE, title, status);
     pjsua_destroy();
     exit(1);
-} 
+}
 /* Callback called by the library upon receiving incoming call */
 static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id, pjsip_rx_data *rdata)
 {
@@ -25,7 +36,7 @@ static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id, pjsip_r
     ci.remote_info.ptr));
     /* Automatically answer incoming calls with 200/OK */
     pjsua_call_answer(call_id, 200, NULL, NULL);
-} 
+}
 /* Callback called by the library when call's state has changed */
 static void on_call_state(pjsua_call_id call_id, pjsip_event *e)
 {
@@ -35,7 +46,7 @@ static void on_call_state(pjsua_call_id call_id, pjsip_event *e)
     PJ_LOG(3,(THIS_FILE, "Call %d state=%.*s", call_id,
     (int)ci.state_text.slen,
     ci.state_text.ptr));
-} 
+}
 /* Callback called by the library when call's media state has changed */
 static void on_call_media_state(pjsua_call_id call_id)
 {
@@ -45,7 +56,7 @@ static void on_call_media_state(pjsua_call_id call_id)
         pjsua_conf_connect(ci.conf_slot, 0);
         pjsua_conf_connect(0, ci.conf_slot);
     }
-} 
+}
 
 int main(int argc, char *argv[])
 {
@@ -67,7 +78,7 @@ int main(int argc, char *argv[])
         log_cfg.console_level = 4; 
         status = pjsua_init(&cfg, &log_cfg, NULL);
         if (status != PJ_SUCCESS) error_exit("Error in pjsua_init()", status);
-    } 
+    }
     /* Add UDP transport. */
     {
         pjsua_transport_config cfg; 
@@ -75,24 +86,60 @@ int main(int argc, char *argv[])
         cfg.port = 5060;
         status = pjsua_transport_create(PJSIP_TRANSPORT_UDP, &cfg, NULL);
         if (status != PJ_SUCCESS) error_exit("Error creating transport", status);
-    } 
+    }
     /* Initialization is done, now start pjsua */
     status = pjsua_start();
 
     /* Register to SIP server by creating SIP account. */
     {
-        pjsua_acc_config cfg; 
-        pjsua_acc_config_default(&cfg);
-        cfg.id = pj_str("sip:" SIP_USER "@" SIP_DOMAIN);
-        cfg.reg_uri = pj_str("sip:" SIP_DOMAIN);
-        cfg.cred_count = 1;
-        cfg.cred_info[0].realm = pj_str((char *)"*");
-        cfg.cred_info[0].scheme = pj_str((char *)"digest");
-        cfg.cred_info[0].username = pj_str(SIP_USER);
-        cfg.cred_info[0].data_type = PJSIP_CRED_DATA_PLAIN_PASSWD;
-        cfg.cred_info[0].data = pj_str(SIP_PASSWD); 
-        status = pjsua_acc_add(&cfg, PJ_TRUE, &acc_id);
-    } 
+        pjsua_acc_config cfg[4];
+
+        pjsua_acc_config_default(&cfg[0]);
+        pjsua_acc_config_default(&cfg[1]);
+        pjsua_acc_config_default(&cfg[2]);
+        pjsua_acc_config_default(&cfg[3]);
+
+        cfg[0].id = pj_str("sip:" SIP_USER_0 "@" SIP_DOMAIN);
+        cfg[0].reg_uri = pj_str("sip:" SIP_DOMAIN);
+        cfg[0].cred_count = 1;
+        cfg[0].cred_info[0].realm = pj_str((char *)"*");
+        cfg[0].cred_info[0].scheme = pj_str((char *)"digest");
+        cfg[0].cred_info[0].username = pj_str(SIP_USER_0);
+        cfg[0].cred_info[0].data_type = PJSIP_CRED_DATA_PLAIN_PASSWD;
+        cfg[0].cred_info[0].data = pj_str(SIP_PASSWD_0);
+
+        cfg[1].id = pj_str("sip:" SIP_USER_1 "@" SIP_DOMAIN);
+        cfg[1].reg_uri = pj_str("sip:" SIP_DOMAIN);
+        cfg[1].cred_count = 1;
+        cfg[1].cred_info[0].realm = pj_str((char *)"*");
+        cfg[1].cred_info[0].scheme = pj_str((char *)"digest");
+        cfg[1].cred_info[0].username = pj_str(SIP_USER_1);
+        cfg[1].cred_info[0].data_type = PJSIP_CRED_DATA_PLAIN_PASSWD;
+        cfg[1].cred_info[0].data = pj_str(SIP_PASSWD_1);
+
+        cfg[2].id = pj_str("sip:" SIP_USER_2 "@" SIP_DOMAIN);
+        cfg[2].reg_uri = pj_str("sip:" SIP_DOMAIN);
+        cfg[2].cred_count = 1;
+        cfg[2].cred_info[0].realm = pj_str((char *)"*");
+        cfg[2].cred_info[0].scheme = pj_str((char *)"digest");
+        cfg[2].cred_info[0].username = pj_str(SIP_USER_2);
+        cfg[2].cred_info[0].data_type = PJSIP_CRED_DATA_PLAIN_PASSWD;
+        cfg[2].cred_info[0].data = pj_str(SIP_PASSWD_2);
+
+        cfg[3].id = pj_str("sip:" SIP_USER_3 "@" SIP_DOMAIN);
+        cfg[3].reg_uri = pj_str("sip:" SIP_DOMAIN);
+        cfg[3].cred_count = 1;
+        cfg[3].cred_info[0].realm = pj_str((char *)"*");
+        cfg[3].cred_info[0].scheme = pj_str((char *)"digest");
+        cfg[3].cred_info[0].username = pj_str(SIP_USER_3);
+        cfg[3].cred_info[0].data_type = PJSIP_CRED_DATA_PLAIN_PASSWD;
+        cfg[3].cred_info[0].data = pj_str(SIP_PASSWD_3);
+
+        status = pjsua_acc_add(&cfg[0], PJ_TRUE, &acc_id);
+        status = pjsua_acc_add(&cfg[1], PJ_TRUE, &acc_id);
+        status = pjsua_acc_add(&cfg[2], PJ_TRUE, &acc_id);
+        status = pjsua_acc_add(&cfg[3], PJ_TRUE, &acc_id);
+    }
 
     /* Wait until user press "q" to quit. */
     for (;;) {
@@ -101,7 +148,7 @@ int main(int argc, char *argv[])
         if (fgets(option, sizeof(option), stdin) == NULL) {
             puts("EOF while reading stdin, will quit now..");
             break;
-        } 
+        }
         if (option[0] == 'q') break; 
         if (option[0] == 'h') pjsua_call_hangup_all();
     }
